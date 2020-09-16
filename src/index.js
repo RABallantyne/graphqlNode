d59aadd6d71f3de649bc359354746468e12dc3ae
Jerkbert <raballantyne@gmail.com>
2020-09-16 14:07:59 -0600

    first query running

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
+import { GraphQLServer } from 'graphql-yoga';
 
+//Type definitions (schema)
+
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+  }
+`;
+
+//Resolvers
+
+const resolvers = {
+  Query: {
+    hello() {
+      return 'This is my first query!';
+    },
+    name() {
+      return 'Bobby B';
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log('Server started!');
+});
