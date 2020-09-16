0cc5bc753cb877b6670c308f3fa7822edcf95e0d
Jerkbert <raballantyne@gmail.com>
2020-09-16 11:42:10 -0600

    clean up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-import currentLoc, { getGreeting, message, name } from './myModule';
-import addErUp, { subtract } from './math';
 
-console.log(message);
-
-console.log(name);
-
-console.log(currentLoc);
-
-console.log(getGreeting('Bobbbboooo'));
-
-console.log(addErUp(5, 4));
-
-console.log(subtract(7, 4));
--- src/math.js
+++
@@ -1,9 +0,0 @@
-const add = (a, b) => {
-  return `Total is: ${a + b}`;
-};
-
-const subtract = (a, b) => {
-  return `Total is: ${a - b}`;
-};
-
-export { add as default, subtract };
